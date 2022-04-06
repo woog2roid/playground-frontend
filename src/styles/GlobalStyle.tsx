9944be802101d9391242deaec89edd2e3d9a10e6
woog2roid <wooguijung@korea.ac.kr>
2022-04-06 14:02:48 +0000

    fix: 브라우저 알림 삭제 및 드래그 방지 기능 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -21,6 +21,12 @@ const style = css`
 
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    -ms-user-select: none; 
+    -moz-user-select: -moz-none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
   }
 
   code {
