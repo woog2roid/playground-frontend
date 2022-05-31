013e372e5d5c0737a8182dde7d9a7f830ecfdb55
woog2roid <wooguijung@korea.ac.kr>
2022-05-31 16:44:48 +0000

    feat: 그룹 채팅방 생성 기능 추가

--- src/components/Chat/ChatRoomList/ChatRoomController/index.tsx
+++ src/components/Chat/ChatRoomList/ChatRoomController/index.tsx
@@ -2,25 +2,34 @@ import * as React from 'react';
 
 import { MarkChatReadOutlined, AddBoxOutlined } from '@mui/icons-material';
 
+import GenerateChatRoomModal from './GenerateChatRoomModal';
 import { Wrapper } from './style';
 
 export default function ChatRoomController() {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   const onClickReadAll = () => {
     console.log('안 읽은 채팅 모두 읽기');
   };
 
-  const onClickAddChat = () => {
-    console.log('새로운 채팅방 만들기');
+  const onClickGenerateChatRoom = () => {
+    openModal();
   };
 
   return (
-    <Wrapper>
-      <span className="icons" onClick={onClickReadAll}>
-        <MarkChatReadOutlined />
-      </span>
-      <span className="icons" onClick={onClickAddChat}>
-        <AddBoxOutlined />
-      </span>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <span className="icons" onClick={onClickReadAll}>
+          <MarkChatReadOutlined />
+        </span>
+        <span className="icons" onClick={onClickGenerateChatRoom}>
+          <AddBoxOutlined />
+        </span>
+      </Wrapper>
+
+      <GenerateChatRoomModal isOpen={isModalOpen} handleClose={closeModal} />
+    </>
   );
 }
